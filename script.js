7dcf4d40c4736490aa00e8ba68c06ab0e12ff765
jdab2020 <jdabalos90@gmail.com>
2020-01-25 22:26:39 -0800

    removed for loop and instead use psuedo loop but with bug that skips showing questions but still executes -shown by console logs

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-var startButton = document.querySelector("#start_button");
-var showTimer = document.querySelector("#hidden1");
-var hideInstructions = document.querySelector("#hidden2");
-var showQuestions = document.querySelector("#hidden3");
-var showHighScoresName = document.querySelector("#hidden4");
+let startButton = document.querySelector("#start_button");
+let showTimer = document.querySelector("#hidden1");
+let hideInstructions = document.querySelector("#hidden2");
+let showQuestions = document.querySelector("#hidden3");
+let toggleHR = document.querySelector("#hidden4");
+let toggleRightWrong = document.querySelector("#hidden5")
+let showHighScoresName = document.querySelector("#hidden6");
 
 // starts timer, shows questions, hides instructions 
 startButton.addEventListener("click", function () {
     setTime();
     hideTheInstructions();
+    writeQuestions();
     showTheQuestions();
 
 });
 
-var timerStarts = document.querySelector("#countdown");
-var secondsLeft = 100;
+let timerStarts = document.querySelector("#countdown");
+let secondsLeft = 100;
 
 function setTime() {
     showTimer.style.display = "block";
@@ -29,14 +32,29 @@ function hideTheInstructions() {
 
 function showTheQuestions() {
     showQuestions.style.display = "block";
-    writeQuestions();
 }
 
 function showTheHighScoresNameInput() {
     showHighScoresName.style.display = "block";
 }
 
-// var questionAnswers = {
+function togglingHR() {
+    if (toggleHR === "hidden") {
+        toggleHR.style.visibility = "visible";
+    }
+    else
+        toggleHR.style.visibility = "hidden";
+}
+
+function togglingRightWrong() {
+    if (toggleRightWrong === "hidden") {
+        toggleRightWrong = "visible";
+    }
+    else
+        toggleRightWrong = "hidden";
+}
+
+// let questionAnswers = {
 //     "This_is_question_1": ["answer", "ans", "ans", "ans"],
 //     "This_is_question_2": ["ans", "answer", "ans", "ans"],
 //     "This_is_question_3": ["ans", "ans", "answer", "ans"],
@@ -47,7 +65,6 @@ function showTheHighScoresNameInput() {
 // }
 // giving up on objects for now; couldn't get it to work properly
 // going to use arrays instead
-
 // function writeQuestions(){
 //     while (secondsLeft > 0){
 //         for (let i = 0; i < Object.keys(questionAnswers).length; i++) {
@@ -60,7 +77,7 @@ function showTheHighScoresNameInput() {
 //     }
 // }
 
-var questionToWrite = [
+let questionToWrite = [
     "This_is_question_1",
     "This_is_question_2",
     "This_is_question_3",
@@ -69,7 +86,7 @@ var questionToWrite = [
     "This_is_question_6",
     "This_is_question_7"];
 
-var answerToQuestions = [
+let answerToQuestions = [
     ["answer", "ans", "ans", "ans"],
     ["ans", "answer", "ans", "ans"],
     ["ans", "ans", "answer", "ans"],
@@ -79,43 +96,103 @@ var answerToQuestions = [
     ["answer", "ans", "ans", "ans"],
 ];
 
-var rightAnswersIdx = [0, 1, 2, 3, 2, 1, 0];
-var quest = document.querySelector("#question");
+let rightAnswersIdx = [0, 1, 2, 3, 2, 1, 0];
+let quest = document.querySelector("#question");
 
-var button0 = document.querySelector("#ans0");
-var button1 = document.querySelector("#ans1");
-var button2 = document.querySelector("#ans2");
-var button3 = document.querySelector("#ans3");
+let button0 = document.querySelector("#ans0");
+let button1 = document.querySelector("#ans1");
+let button2 = document.querySelector("#ans2");
+let button3 = document.querySelector("#ans3");
+let index = 0;
 
+// Had a for loop inside the function but wouldn't wait for eventListener before each iteration
+// New bug... shows first question then second then skips to fourth then skips to seventh
+// Toggling the horizontal line and right or wrong answer doesn't work
 function writeQuestions() {
-//    while (secondsLeft > 0) {
-        for (let i = 0; i < questionToWrite.length; i++) {
-            quest.textContent = questionToWrite[i];
-            button0.textContent = answerToQuestions[i][0];
-            button1.textContent = answerToQuestions[i][1];
-            button2.textContent = answerToQuestions[i][2];
-            button3.textContent = answerToQuestions[i][3];
-            userAnswers();
-        }
-//    }
+    if (index < questionToWrite.length) {
+        quest.textContent = questionToWrite[index];
+        button0.textContent = answerToQuestions[index][0];
+        button1.textContent = answerToQuestions[index][1];
+        button2.textContent = answerToQuestions[index][2];
+        button3.textContent = answerToQuestions[index][3];
+        console.log(quest.textContent);
+        console.log(button0.textContent);
+        console.log(button1.textContent);
+        console.log(button2.textContent);
+        console.log(button3.textContent);
+        userAnswers();
+    }
+    
 }
 
 function userAnswers() {
     button0.addEventListener("click", function (event) {
         event.preventDefault();
-    
+        if (button0.value === answerToQuestions[index][rightAnswersIdx[index]]) {
+            toggleRightWrong.textContent = "Correct"
+            togglingHR();
+            togglingRightWrong();
+            index++
+        }
+        else {
+            toggleRightWrong.textContent = "Wrong"
+            togglingHR();
+            togglingRightWrong();
+            index++
+        }
+
+        writeQuestions();
     });
     button1.addEventListener("click", function (event) {
         event.preventDefault();
-    
+        if (button1.value === answerToQuestions[index][rightAnswersIdx[index]]) {
+            toggleRightWrong.textContent = "Correct"
+            togglingHR();
+            togglingRightWrong();
+            index++
+        }
+        else {
+            toggleRightWrong.textContent = "Wrong"
+            togglingHR();
+            togglingRightWrong();
+            index++
+        }
+
+        writeQuestions();
     });
     button2.addEventListener("click", function (event) {
         event.preventDefault();
-    
+        if (button2.value === answerToQuestions[index][rightAnswersIdx[index]]) {
+            toggleRightWrong.textContent = "Correct"
+            togglingHR();
+            togglingRightWrong();
+            index++
+        }
+        else {
+            toggleRightWrong.textContent = "Wrong"
+            togglingHR();
+            togglingRightWrong();
+            index++
+        }
+
+        writeQuestions();
     });
     button3.addEventListener("click", function (event) {
         event.preventDefault();
+        if (button3.value === answerToQuestions[index][rightAnswersIdx[index]]) {
+            toggleRightWrong.textContent = "Correct"
+            togglingHR();
+            togglingRightWrong();
+            index++
+        }
+        else {
+            toggleRightWrong.textContent = "Wrong"
+            togglingHR();
+            togglingRightWrong();
+            index++
+        }
 
+        writeQuestions();
     });
 
 }
