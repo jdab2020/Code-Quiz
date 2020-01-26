70eb17b88eec28d9f4317986a99c5b5c6488ecf4
jdab2020 <jdabalos90@gmail.com>
2020-01-26 14:17:48 -0800

    took out toggling / added and updated bootstrap css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let startButton = document.querySelector("#start_button");
 let showTimer = document.querySelector("#hidden1");
 let hideInstructions = document.querySelector("#hidden2");
 let showQuestions = document.querySelector("#hidden3");
-let toggleHR = document.querySelector("#hidden4");
+// let toggleHR = document.querySelector("#hidden4");
 let toggleRightWrong = document.querySelector("#hidden5")
 let showHighScoresName = document.querySelector("#hidden6");
 
@@ -38,47 +38,6 @@ function showTheHighScoresNameInput() {
     showHighScoresName.style.display = "block";
 }
 
-function togglingHR() {
-    //    toggleHR.style.visibility = "visible";
-    if (toggleHR.style.visibility === "hidden") {
-        toggleHR.style.visibility = "visible";
-    }
-    else
-        toggleHR.style.visibility = "hidden";
-}
-
-function togglingRightWrong() {
-    //    toggleRightWrong.style.visibility = "visible";
-    if (toggleRightWrong.style.visibility === "hidden") {
-        toggleRightWrong.style.visibility = "visible";
-    }
-    else
-        toggleRightWrong.style.visibility = "hidden";
-}
-
-// let questionAnswers = {
-//     "This_is_question_1": ["answer", "ans", "ans", "ans"],
-//     "This_is_question_2": ["ans", "answer", "ans", "ans"],
-//     "This_is_question_3": ["ans", "ans", "answer", "ans"],
-//     "This_is_question_4": ["ans", "ans", "ans", "answer"],
-//     "This_is_question_5": ["ans", "ans", "answer", "ans"],
-//     "This_is_question_6": ["ans", "answer", "ans", "ans"],
-//     "This_is_question_7": ["answer", "ans", "ans", "ans"],
-// }
-// giving up on objects for now; couldn't get it to work properly
-// going to use arrays instead
-// function writeQuestions(){
-//     while (secondsLeft > 0){
-//         for (let i = 0; i < Object.keys(questionAnswers).length; i++) {
-//             quest.textContent = Object.keys(questionAnswers)[i];         
-//             button0.textContent = Object.values(questionAnswers);
-//             button1.textContent = Object.values(questionAnswers);
-//             button2.textContent = Object.values(questionAnswers);
-//             button3.textContent = Object.values(questionAnswers);
-//         }
-//     }
-// }
-
 let questionToWrite = [
     "This_is_question_1",
     "This_is_question_2",
@@ -106,41 +65,25 @@ let button1 = document.querySelector("#ans1");
 let button2 = document.querySelector("#ans2");
 let button3 = document.querySelector("#ans3");
 let index = 0;
+let score = 0;
 
 // Had a for loop inside the function but wouldn't wait for eventListener before each iteration
 // New bug... shows first question then second then skips to fourth then skips to seventh
 // Toggling the horizontal line and right or wrong answer doesn't work
+// Removed toggling - going to replace it something else...
 function writeQuestions() {
-    // setTimeout(() => {
-        
-    // }, 2000);
-    // if (toggleRightWrong.style.visibility === "visible") {
-    //     toggleRightWrong.style.visibility = "hidden";
-    // };
-    // if (toggleHR.style.visibility === "visible") {
-    //     toggleHR.style.visibility = "hidden";
-    // };
-    if (index < questionToWrite.length) {
+    if (index < questionToWrite.length && secondsLeft > 0) {
         quest.textContent = questionToWrite[index];
         button0.textContent = answerToQuestions[index][0];
         button1.textContent = answerToQuestions[index][1];
         button2.textContent = answerToQuestions[index][2];
         button3.textContent = answerToQuestions[index][3];
-        // console.log(quest.textContent);
-        // console.log(button0.textContent);
-        // console.log(button1.textContent);
-        // console.log(button2.textContent);
-        // console.log(button3.textContent);
     }
     else {
         showQuestions.style.display = "none";
         showTimer.style.display = "none";
         showTheHighScoresNameInput()
     };
-    // toggleHR.style.visibility = "hidden";
-    // toggleRightWrong.style.visibility = "hidden";
-    // console.log(toggleHR.style.visibility);
-    // console.log(toggleRightWrong.style.visibility);
  }
 
 // Removed the button click event handlers out of function 
@@ -148,101 +91,56 @@ button0.addEventListener("click", function (event) {
     event.preventDefault();
     if (button0.textContent === answerToQuestions[index][rightAnswersIdx[index]]) {
         toggleRightWrong.textContent = "Correct";
-        togglingHR();
-        togglingRightWrong();
+        score++
+        console.log(toggleRightWrong.textContent);
+        console.log(toggleRightWrong.style.visibility)
     }
     else {
         toggleRightWrong.textContent = "Wrong";
-        togglingHR();
-        togglingRightWrong();
+        secondsLeft = secondsLeft-10;
     };
-    // if (toggleRightWrong.style.visibility === "hidden") {
-    //     toggleRightWrong.style.visibility = "visible";
-    // };
     index++
-    console.log(toggleHR.style.visibility);
-    console.log(toggleRightWrong.style.visibility);
-    console.log(toggleRightWrong.textContent);
-    togglingHR();
-    togglingRightWrong();
-    console.log(toggleHR.style.visibility);
-    console.log(toggleRightWrong.style.visibility);
-    writeQuestions();
+    // writeQuestions();
 
 });
 button1.addEventListener("click", function (event) {
     event.preventDefault();
     if (button1.textContent === answerToQuestions[index][rightAnswersIdx[index]]) {
         toggleRightWrong.textContent = "Correct";
-        togglingHR();
-        togglingRightWrong();
+        score++
     }
     else {
         toggleRightWrong.textContent = "Wrong";
-        togglingHR();
-        togglingRightWrong();
+        secondsLeft = secondsLeft-10;
     };
-    // if (toggleRightWrong.style.visibility === "hidden") {
-    //     toggleRightWrong.style.visibility = "visible";
-    // };
     index++
-    console.log(toggleHR.style.visibility);
-    console.log(toggleRightWrong.style.visibility);
-    console.log(toggleRightWrong.textContent);
-    togglingHR();
-    togglingRightWrong();
-    console.log(toggleHR.style.visibility);
-    console.log(toggleRightWrong.style.visibility);
-    writeQuestions();
+    // writeQuestions();
 });
 button2.addEventListener("click", function (event) {
     event.preventDefault();
     if (button2.textContent === answerToQuestions[index][rightAnswersIdx[index]]) {
         toggleRightWrong.textContent = "Correct";
-        togglingHR();
-        togglingRightWrong();
+        score++
     }
     else {
         toggleRightWrong.textContent = "Wrong";
-        togglingHR();
-        togglingRightWrong();
+        secondsLeft = secondsLeft-10;
     };
-    // if (toggleRightWrong.style.visibility === "hidden") {
-    //     toggleRightWrong.style.visibility = "visible";
-    // };
     index++
-    console.log(toggleHR.style.visibility);
-    console.log(toggleRightWrong.style.visibility);
-    console.log(toggleRightWrong.textContent);
-    togglingHR();
-    togglingRightWrong();
-    console.log(toggleHR.style.visibility);
-    console.log(toggleRightWrong.style.visibility);
-    writeQuestions();
+    // writeQuestions();
 
 });
 button3.addEventListener("click", function (event) {
     event.preventDefault();
     if (button3.textContent === answerToQuestions[index][rightAnswersIdx[index]]) {
         toggleRightWrong.textContent = "Correct";
-        togglingHR();
-        togglingRightWrong();
+        score++
     }
     else {
         toggleRightWrong.textContent = "Wrong";
-        togglingHR();
-        togglingRightWrong();
+        secondsLeft = secondsLeft-10;
+
     };
-    // if (toggleRightWrong.style.visibility === "hidden") {
-    //     toggleRightWrong.style.visibility = "visible";
-    // };
     index++
-    console.log(toggleHR.style.visibility);
-    console.log(toggleRightWrong.style.visibility);
-    console.log(toggleRightWrong.textContent);
-    togglingHR();
-    togglingRightWrong();
-    console.log(toggleHR.style.visibility);
-    console.log(toggleRightWrong.style.visibility);
-    writeQuestions();
+    // writeQuestions();
 });
